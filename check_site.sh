b1eabaa0c38cc75e7fe001da2c76d1cb75f86d15
Reda Belalioui <redabelalioui0@gmail.com>
2025-12-27 01:42:37 +0100

    Add CI pipeline

---
+++ check_site.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+target=$1
+
+if [ -z "$1" ]; then
+        echo "Please provide a web site to check."
+        echo "Usage: ./check_site.sh <domain>"
+        exit 1
+fi
+
+echo "Checking $target..."
+
+if curl -I -L "$target" 2>&1 | grep -q "200 OK"; then
+        echo "The site is UP!"
+else
+        echo "The site is DOWN!"
+fi
